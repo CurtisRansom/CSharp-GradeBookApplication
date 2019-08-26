b439a1990e6e9f918b3286c801eb85fd32803af8
cransom <cransom@babelstreet.com>
2019-08-26 14:54:03 -0400

    3.2 through 3.5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -13,9 +14,37 @@ namespace GradeBook.GradeBooks
         {
             if(this.Students.Count < 5)
             {
+                Console.WriteLine("Your grading book requires at least 5 students");
                 throw new InvalidOperationException();
             }
-            return 'F';
+
+            // Calculate your threshold, the number of students equating to 20% of the total
+            var threshold = (int)Math.Ceiling(Students.Count * .2);
+
+            // Get all the average grades in descending order
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if(grades[threshold-1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[threshold * 2 - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[threshold * 3 - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[threshold * 4 - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+ 
         }
     }
 }
