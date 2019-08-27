a7b4085d73f856de7143264f124f659fca2de45d
cransom <cransom@babelstreet.com>
2019-08-27 15:35:47 -0400

    7.1 and 7.2

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -16,9 +16,12 @@ namespace GradeBook.GradeBooks
 
         public GradeBookType Type { get; set; }
 
-        public BaseGradeBook(string name)
+        public bool IsWeighted { get; set; }
+
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = IsWeighted;
             Students = new List<Student>();
         }
 
